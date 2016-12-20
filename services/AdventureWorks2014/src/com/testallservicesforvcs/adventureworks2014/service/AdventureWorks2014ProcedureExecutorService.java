3acd8a542a27aeeebb3bb6467019fb7333fcc971
root <root@58351ae9216c>
2016-12-20 06:41:33 +0000

    Initial Push

---
+++ services/AdventureWorks2014/src/com/testallservicesforvcs/adventureworks2014/service/AdventureWorks2014ProcedureExecutorService.java
@@ -0,0 +1,24 @@
+/*Copyright (c) 2016-2017 vcstest4.com All Rights Reserved.
+ This software is the confidential and proprietary information of vcstest4.com You shall not disclose such Confidential Information and shall use it only in accordance
+ with the terms of the source code license agreement you entered into with vcstest4.com*/
+
+package com.testallservicesforvcs.adventureworks2014.service;
+
+import java.util.Map;
+import java.util.List;
+
+
+import com.wavemaker.runtime.data.model.CustomProcedure;
+import com.wavemaker.runtime.data.exception.QueryParameterMismatchException;
+
+public interface AdventureWorks2014ProcedureExecutorService {
+
+    List<Object> executeGetdeptInfo(java.lang.Integer deptid) throws QueryParameterMismatchException;
+
+
+	
+	List<Object> executeWMCustomProcedure(CustomProcedure procedure) ;
+
+
+}
+
---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "TestAllServicesforVCS",
+  "homePage" : "Main",
+  "name" : "TestAllServicesforVCS",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
